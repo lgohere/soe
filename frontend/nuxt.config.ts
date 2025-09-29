391a74f4ea6b1bb0827ca2abe6f5f7e776e76f48
Luiz Gouveia <lcpgou@gmail.com>
2025-09-29 14:56:21 -0300

    favicon manifest updated2

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -50,11 +50,11 @@ export default defineNuxtConfig({
         { name: 'language', content: 'pt-BR' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=202509' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=202509' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=202509' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=202509' },
+        { rel: 'manifest', href: '/site.webmanifest?v=202509' },
         { rel: 'canonical', href: 'https://soe.texts.com.br' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
